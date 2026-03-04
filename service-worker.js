9560a97c03c591aeffbc24fad43cbe873afcc5a7
krc-nz <kaipatikirepaircafes@gmail.com>
2026-03-04 22:13:39 +1300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'repair-cafe-v1';
+const urlsToCache = [
+  '/repair-cafe-dashboard/',
+  '/repair-cafe-dashboard/index.html',
+  '/repair-cafe-dashboard/banner.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
